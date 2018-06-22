b634b7138b4e8ff8ff53b0223ed5735fcbd1d7b1
Craig <craig.eddy@gmail.com>
2018-06-22 18:56:09 -0400

    Added Stock Entry component

--- src/app/inventory/inventory.module.ts
+++ src/app/inventory/inventory.module.ts
@@ -5,9 +5,10 @@ import { MaterialModule } from '../material.module'
 import { InventoryRoutingModule } from './inventory-routing.module'
 import { InventoryComponent } from './inventory.component'
 import { InventoryHomeComponent } from './inventory-home/inventory-home.component'
+import { StockEntryComponent } from './stock-entry/stock-entry.component'
 
 @NgModule({
   imports: [CommonModule, InventoryRoutingModule],
-  declarations: [InventoryComponent, InventoryHomeComponent],
+  declarations: [InventoryComponent, InventoryHomeComponent, StockEntryComponent],
 })
 export class InventoryModule {}
