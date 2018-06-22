cd225826d780ac11fd241daca761fd0794328ac3
Craig <craig.eddy@gmail.com>
2018-06-22 18:54:10 -0400

    Added inventory component

--- src/app/inventory/inventory.module.ts
+++ src/app/inventory/inventory.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
+import { MaterialModule } from '../material.module'
 
 import { InventoryRoutingModule } from './inventory-routing.module'
+import { InventoryComponent } from './inventory.component'
 
 @NgModule({
   imports: [CommonModule, InventoryRoutingModule],
-  declarations: [],
+  declarations: [InventoryComponent],
 })
 export class InventoryModule {}
