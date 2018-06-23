ed5be398483cce01e5f93e1c32b48bc8b5c4afbd
Craig <craig.eddy@gmail.com>
2018-06-23 06:35:31 -0400

    Removed module imports for modules that should have been lazy loaded

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,6 @@ import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { MaterialModule } from './/material.module'
-import { InventoryModule } from './inventory/inventory.module'
-import { PosModule } from './pos/pos.module'
-import { UserModule } from './user/user.module'
 import { HomeComponent } from './home/home.component'
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component'
 import { HttpClientModule } from '@angular/common/http'
@@ -20,9 +17,6 @@ import { FlexLayoutModule } from '@angular/flex-layout'
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
-    InventoryModule,
-    PosModule,
-    UserModule,
     HttpClientModule,
     FlexLayoutModule,
   ],
