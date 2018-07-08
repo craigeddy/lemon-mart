4584dcd302f3dfd1d5724f980f3181b94e67b0dd
Craig <craig.eddy@gmail.com>
2018-07-08 09:07:28 -0400

    Added regex password validation

--- src/app/common/validations.ts
+++ src/app/common/validations.ts
@@ -2,8 +2,10 @@ import { Validators, RequiredValidator } from '@angular/forms'
 
 export const EmailValidation = [Validators.required, Validators.email]
 
+/** Password validators */
 export const PasswordValidation = [
   Validators.required,
   Validators.minLength(8),
   Validators.maxLength(50),
+  Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*d)(?=.*[!@#$%^&])[^ ]{8,}$'),
 ]
